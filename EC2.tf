60e390de90164515c28034d6c8c74a1d8f757bd2
Poojasaykar <psaykar75@gmail.com>
2025-12-19 10:53:39 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,14 +1,13 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAxxxxxxxxxxxx"
-  secret_key = "xxxxxxxxxxxxxxxxxxxxxxxx"
 }
 
 resource "aws_instance" "test_ec2" {
+    count = 3
   ami           = "ami-0f5ee92e2d63afc18"   # Amazon Linux 2 (Mumbai)
   instance_type = "t2.micro"
 
   tags = {
-    Name = "terraform-test-ec2"
+    Name = "terraform-test-ec2${count.index+1}"
   }
 }

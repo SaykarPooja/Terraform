56531449ab66f82e5213dc56c751a789f7f648ea
Poojasaykar <psaykar75@gmail.com>
2025-12-20 21:12:00 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "test_ec2" {
     count = 3
-  ami           = "ami-0f5ee92e2d63afc18"   # Amazon Linux 2 (Mumbai)
+  ami           = var.instance_image   # Amazon Linux 2 (Mumbai)
   instance_type = "t2.micro"
 
   tags = {

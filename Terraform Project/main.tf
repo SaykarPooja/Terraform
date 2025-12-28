ea2efe09a6520c17edd01c911db76197d6d62335
Poojasaykar <psaykar75@gmail.com>
2025-12-28 22:33:09 +0530

    Update main.tf

--- Terraform Project/main.tf
+++ Terraform Project/main.tf
@@ -1,23 +1,29 @@
 resource "aws_vpc" "myvpc" {
-    cidr_block = var.cidr
-    tags = {
-      name="MYVPC"
-    }
+  cidr_block = var.cidr
+  tags = {
+    name = "MYVPC"
+  }
 }
 resource "aws_subnet" "sub1" {
-    vpc_id = aws_vpc.myvpc.id
-   cidr_block = "10.0.0.0/24"
-   availability_zone = "ap-south-1a"
-   map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    name = "sub1"
+  }
 }
 resource "aws_subnet" "sub2" {
-    vpc_id = aws_vpc.myvpc.id
-   cidr_block = "10.0.1.0/24"
-   availability_zone = "ap-south-1b"
-   map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    name = "sub2"
+  }
 }
 resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.myvpc.id
+  vpc_id = aws_vpc.myvpc.id
 }
 resource "aws_route_table" "route" {
   vpc_id = aws_vpc.myvpc.id
@@ -28,16 +34,16 @@ resource "aws_route_table" "route" {
 }
 
 resource "aws_route_table_association" "rta1" {
-   subnet_id      = aws_subnet.sub1.id
-   route_table_id = aws_route_table.route.id
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.route.id
 }
 resource "aws_route_table_association" "rta2" {
-   subnet_id      = aws_subnet.sub2.id
-   route_table_id = aws_route_table.route.id
+  subnet_id      = aws_subnet.sub2.id
+  route_table_id = aws_route_table.route.id
 }
 resource "aws_security_group" "sg" {
   name_prefix = "aws_sg"
-  vpc_id = aws_vpc.myvpc.id
+  vpc_id      = aws_vpc.myvpc.id
 
   ingress {
     from_port   = 22
@@ -62,17 +68,78 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_s3_bucket" "s3" {
-  bucket = "awsbtspooja3"  
+  bucket = "awsbtspooja3"
+  tags = {
+    name = "bkt_1"
+  }
 }
 resource "aws_instance" "server1" {
-  ami= "ami-02b8269d5e85954ef"
-  instance_type = "t3.micro"
+  ami                    = "ami-02b8269d5e85954ef"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id = aws_subnet.sub1.id  
+  subnet_id              = aws_subnet.sub1.id
+  user_data              = base64encode(file("userdata1.sh"))
+  tags = {
+    Name = "server1"
+  }
 }
 resource "aws_instance" "server2" {
-  ami= "ami-02b8269d5e85954ef"
-  instance_type = "t3.micro"
+  ami                    = "ami-02b8269d5e85954ef"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id = aws_subnet.sub2.id  
+  subnet_id              = aws_subnet.sub2.id
+  user_data              = base64encode(file("userdata2.sh"))
+  tags = {
+    Name = "server2"
+  }
+}
+resource "aws_lb" "ALB" {
+  name               = "MYALB"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [aws_security_group.sg.id]
+  subnets         = [aws_subnet.sub1.id, aws_subnet.sub2.id]
+  tags = {
+    name = "ALB"
+  }
+
+}
+resource "aws_lb_target_group" "lb_grp" {
+  name     = "MyTG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.myvpc.id
+
+  health_check {
+    path = "/"
+    port = "traffic-port"
+
+  }
+}
+resource "aws_lb_target_group_attachment" "attach1" {
+  target_group_arn = aws_lb_target_group.lb_grp.arn
+  target_id        = aws_instance.server1.id
+  port             = 80
+}
+resource "aws_lb_target_group_attachment" "attach2" {
+  target_group_arn = aws_lb_target_group.lb_grp.arn
+  target_id        = aws_instance.server2.id
+  port             = 80
+}
+resource "aws_lb_listener" "listnr" {
+  load_balancer_arn = aws_lb.ALB.arn   # <- This must be the ALB ARN
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb_grp.arn
+  }
+}
+
+
+output "loadbalancerdns" {
+  value = aws_lb.ALB.dns_name
+
 }
